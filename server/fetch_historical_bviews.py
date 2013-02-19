9bec7c9db88a6e137b97e20bfd2bdc5ca4a66dbe
Raphaël Vinot <raphael@vinot.info>
2013-02-19 10:14:50 +0100

    Add support for the old URL format of the ripe dumps

--- server/fetch_historical_bviews.py
+++ server/fetch_historical_bviews.py
@@ -52,6 +52,8 @@ interval_first = None
 interval_last = None
 
 base_url = 'http://data.ris.ripe.net/rrc00/{year_month}/bview.{file_day}.0000.gz'
+# Until the 2010-06-14, the last update of a day was at
+base_url_old = 'http://data.ris.ripe.net/rrc00/{year_month}/bview.{file_day}.2359.gz'
 filename = 'bview.{day}.gz'
 
 def check_dirs():
@@ -103,17 +105,21 @@ def to_download():
     """
     first_day = parse(interval_first)
     last_day = parse(interval_last)
+    format_change = parse('2010-06-14')
     one_day = datetime.timedelta(1)
     cur_day = first_day
     url_list = []
     while cur_day < last_day:
         fname = filename.format(day = cur_day.strftime("%Y%m%d"))
-        cur_day += one_day
-        url = base_url.format(year_month = cur_day.strftime("%Y.%m"),
-                file_day = cur_day.strftime("%Y%m%d"))
+        if cur_day > format_change:
+            cur_day += one_day
+            url = base_url.format(year_month = cur_day.strftime("%Y.%m"),
+                    file_day = cur_day.strftime("%Y%m%d"))
+        else:
+            url = base_url_old.format(year_month = cur_day.strftime("%Y.%m"),
+                    file_day = cur_day.strftime("%Y%m%d"))
         url_list.append((fname, url))
     return sorted(url_list, key=lambda tup: tup[0], reverse=True)
-
 
 if __name__ == '__main__':
     check_dirs()
