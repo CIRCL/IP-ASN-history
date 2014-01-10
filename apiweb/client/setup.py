bd32f8b2b75747ac5b33f11a23b5a9f2d97c30ae
Raphaël Vinot <raphael@vinot.info>
2014-01-10 16:44:23 +0100

    Make packages PyPi friendly

--- apiweb/client/setup.py
+++ apiweb/client/setup.py
@@ -9,15 +9,27 @@ except ImportError:
 
 setup(
     name='ipasn_web',
-    version='1.0.1',
+    version='1.0.2',
     description='Library to access the IP ASN History REST API.',
     url='https://github.com/CIRCL/IP-ASN-history',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
-    maintainer_email='raphael.vinot@circl.lu',
     packages=['ipasn'],
     cmdclass = {'build_py': build_py},
-    license='GNU GPLv3',
     long_description=open('README.md').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        ],
+
+    requires=['requests']
+
     )
--- client/setup.py
+++ client/setup.py
@@ -2,17 +2,36 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+
 setup(
     name='ip_asn_history',
-    version='1.0.1',
+    version='1.0.3',
     description='API to access an IP-ASN-history instance.',
     url='https://github.com/CIRCL/IP-ASN-history',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
-    maintainer_email='raphael.vinot@circl.lu',
     packages=['ip_asn_history'],
+    cmdclass = {'build_py': build_py},
     scripts = ['ip2asn_asn', 'ip2asn_fullhistory'],
-    license='GNU AGPLv3',
     long_description=open('../README.md').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        ],
+
+    requires=['redis']
+
     )
