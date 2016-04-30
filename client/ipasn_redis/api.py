f9fc3bd85e93c9e869b6859f87443bed0fc8289b
Raphaël Vinot <raphael@vinot.info>
2016-05-01 01:06:44 +0200

    Make the API a class

--- client/ipasn_redis/api.py
+++ client/ipasn_redis/api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import redis
@@ -7,202 +7,146 @@ try:
 except ImportError:  # will be 3.x series
     pass
 
-use_unix_socket = False
 
-hostname = '127.0.0.1'
-unixsocketfile = '/tmp/redis.sock'
-port = 6390
-redis_db = 0
+class IPASN(object):
 
-skip_exception = True
-
-__current_announce_date = None
-__default_announce_date = None
-__number_of_days = -1
-__routing_db = None
-__keys = []
-__netmasks = [
-             [128, 0, 0, 0],
-             [192, 0, 0, 0],
-             [224, 0, 0, 0],
-             [240, 0, 0, 0],
-             [248, 0, 0, 0],
-             [252, 0, 0, 0],
-             [254, 0, 0, 0],
-             [255, 0, 0, 0],
-             [255, 128, 0, 0],
-             [255, 192, 0, 0],
-             [255, 224, 0, 0],
-             [255, 240, 0, 0],
-             [255, 248, 0, 0],
-             [255, 252, 0, 0],
-             [255, 254, 0, 0],
-             [255, 255, 0, 0],
-             [255, 255, 128, 0],
-             [255, 255, 192, 0],
-             [255, 255, 224, 0],
-             [255, 255, 240, 0],
-             [255, 255, 248, 0],
-             [255, 255, 252, 0],
-             [255, 255, 254, 0],
-             [255, 255, 255, 0],
-             [255, 255, 255, 128],
-             [255, 255, 255, 192],
-             [255, 255, 255, 224],
-             [255, 255, 255, 240],
-             [255, 255, 255, 248],
-             [255, 255, 255, 252]]
-
-
-def get_db():
-    global __routing_db
-    if __routing_db is None or not __routing_db.ping():
-        if use_unix_socket:
-            __routing_db = redis.Redis(unix_socket_path=unixsocketfile, db=redis_db)
+    def __init__(self, socket=None, host=None, port=None, db=0, skip_exception=True):
+        if not socket and not host:
+            raise Exception('You need to provide at least a socket or a host')
+        if socket:
+            self.routing_db = redis.Redis(unix_socket_path=socket, db=db)
         else:
-            __routing_db = redis.Redis(host=hostname, port=port, db=redis_db)
-    return __routing_db
+            self.routing_db = redis.Redis(host=host, port=port, db=db)
+        self.db = db
+        self.skip_exception = skip_exception
+        self.update_dates()
+        self.current_announce_date = self.default_announce_date
+        self.netmasks = [[128, 0, 0, 0], [192, 0, 0, 0], [224, 0, 0, 0], [240, 0, 0, 0],
+                         [248, 0, 0, 0], [252, 0, 0, 0], [254, 0, 0, 0], [255, 0, 0, 0],
+                         [255, 128, 0, 0], [255, 192, 0, 0], [255, 224, 0, 0], [255, 240, 0, 0],
+                         [255, 248, 0, 0], [255, 252, 0, 0], [255, 254, 0, 0], [255, 255, 0, 0],
+                         [255, 255, 128, 0], [255, 255, 192, 0], [255, 255, 224, 0], [255, 255, 240, 0],
+                         [255, 255, 248, 0], [255, 255, 252, 0], [255, 255, 254, 0], [255, 255, 255, 0],
+                         [255, 255, 255, 128], [255, 255, 255, 192], [255, 255, 255, 224],
+                         [255, 255, 255, 240], [255, 255, 255, 248], [255, 255, 255, 252]]
 
+    def update_dates(self):
+        self.number_of_days = self.routing_db.scard('imported_dates')
+        self.default_announce_date = sorted(self.routing_db.smembers('imported_dates'), reverse=True)[0]
 
-def __update_default_announce_date():
-    global __default_announce_date
-    global __number_of_days
-    global __routing_db
-    __routing_db = get_db()
-    new_number_of_days = __routing_db.scard('imported_dates')
-    if new_number_of_days != __number_of_days:
-        __number_of_days = new_number_of_days
-        dates = sorted(__routing_db.smembers('imported_dates'), reverse=True)
-        if len(dates) > 0:
-            __default_announce_date = dates[0]
-
-
-def __prepare_keys(ip):
-    global __keys
-    try:
-        __keys = [ip + '/32']
-        ip_split = [int(digit) for digit in ip.split('.')]
-        for mask in reversed(list(range(30))):
-            tmpip = [str(a & b) for a, b in zip(ip_split, __netmasks[mask])]
-            __keys.append('.'.join(tmpip) + '/' + str(mask + 1))
-    except Exception as e:
-        __keys = []
-        if not skip_exception:
-            raise e
-
-
-def get_current_date():
-    """
-        Return the date the query has been using.
-
-        :rtype: String, date, Format: YYYY-MM-DD
-    """
-    return __current_announce_date
-
-
-def get_announce_date(announce_date):
-    if announce_date is None:
-        __update_default_announce_date()
-        announce_date = __default_announce_date
-    elif not __routing_db.sismember('imported_dates', announce_date):
-        dates = __routing_db.smembers('imported_dates')
+    def prepare_keys(self, ip):
+        keys = [ip + '/32']
         try:
-            announce_date = min(enumerate(dates),
-                                key=lambda x: abs(int(x[1]) - int(announce_date)))[1]
-        except:
-            announce_date = __default_announce_date
-        if not skip_exception:
-            raise Exception("unknown date")
-    return announce_date
+            ip_split = [int(digit) for digit in ip.split('.')]
+            ziped = zip([zip(ip_split, m) for m in reversed(self.netmasks)], range(30, 0, -1))
+            keys += ['{}.{}.{}.{}/{mask}'.format(mask=mask, *[x & z for x, z in val]) for val, mask in ziped]
+        except Exception as e:
+            if not self.skip_exception:
+                raise e
+        return keys
 
+    def get_current_date(self):
+        """
+            Return the date the query has been using.
 
-def __run(ip, announce_date=None):
-    global __routing_db
-    __routing_db = get_db()
-    announce_date = get_announce_date(announce_date)
-    __prepare_keys(ip)
-    p = __routing_db.pipeline(False)
-    [p.hget(k, announce_date) for k in __keys]
-    return p.execute(), announce_date
+            :rtype: String, date, Format: YYYY-MM-DD
+        """
+        return self.current_announce_date
 
+    def get_announce_date(self, announce_date):
+        if announce_date is None:
+            self.update_dates()
+            announce_date = self.default_announce_date
+        elif not self.routing_db.sismember('imported_dates', announce_date):
+            dates = self.routing_db.smembers('imported_dates')
+            try:
+                announce_date = min(enumerate(dates), key=lambda x: abs(int(x[1]) - int(announce_date)))[1]
+            except:
+                announce_date = self.default_announce_date
+            if not self.skip_exception:
+                raise Exception("unknown date")
+        return announce_date
 
-def asn(ip, announce_date=None):
-    """
-        Give an IP, maybe a date, get the ASN.
-        This is the fastest command.
+    def run(self, ip, announce_date=None):
+        announce_date = self.get_announce_date(announce_date)
+        keys = self.prepare_keys(ip)
+        p = self.routing_db.pipeline(False)
+        [p.hget(k, announce_date) for k in keys]
+        return p.execute(), announce_date, keys
 
-        :param ip: IP address to search for
-        :param announce_date: Date of the announcement
+    def asn(self, ip, announce_date=None):
+        """
+            Give an IP, maybe a date, get the ASN.
+            This is the fastest command.
 
-        :rtype: String, ASN.
+            :param ip: IP address to search for
+            :param announce_date: Date of the announcement
 
-    """
-    assignations, announce_date = __run(ip, announce_date)
-    return next((assign for assign in assignations if assign is not None), None), announce_date
+            :rtype: String, ASN.
 
+        """
+        assignations, announce_date, _ = self.run(ip, announce_date)
+        return next((assign for assign in assignations if assign is not None), None), announce_date
 
-def date_asn_block(ip, announce_date=None):
-    """
-        Get the ASN and the IP Block announcing the IP at a specific date.
+    def date_asn_block(self, ip, announce_date=None):
+        """
+            Get the ASN and the IP Block announcing the IP at a specific date.
 
-        :param ip: IP address to search for
-        :param announce_date: Date of the announcement
+            :param ip: IP address to search for
+            :param announce_date: Date of the announcement
 
-        :rtype: tuple
+            :rtype: tuple
 
-            .. code-block:: python
+                .. code-block:: python
 
-                (announce_date, asn, block)
+                    (announce_date, asn, block)
 
-        .. note::
+            .. note::
 
-            the returned announce_date might be different of the one
-            given in parameter because some raw files are missing and we
-            don't have the information. In this case, the nearest known
-            date will be chosen,
-    """
-    assignations, announce_date = __run(ip, announce_date)
-    pos = next((i for i, j in enumerate(assignations) if j is not None), None)
-    if pos is not None:
-        block = __keys[pos]
-        if block != '0.0.0.0/0':
-            asn = assignations[pos]
-            return announce_date, asn, block
-    return None
+                the returned announce_date might be different of the one
+                given in parameter because some raw files are missing and we
+                don't have the information. In this case, the nearest known
+                date will be chosen,
+        """
+        assignations, announce_date, keys = self.run(ip, announce_date)
+        pos = next((i for i, j in enumerate(assignations) if j is not None), None)
+        if pos is not None:
+            block = keys[pos]
+            if block != '0.0.0.0/0':
+                return announce_date, assignations[pos], block
+        return None
 
+    def history(self, ip, days_limit=None):
+        """
+            Get the full history of an IP. It takes time.
 
-def history(ip, days_limit=None):
-    """
-        Get the full history of an IP. It takes time.
+            :param ip: IP address to search for
+            :param days_limit: Max amount of days to query. (None means no limit)
 
-        :param ip: IP address to search for
-        :param days_limit: Max amount of days to query. (None means no limit)
+            :rtype: list. For each day in the database: day, asn, block
+        """
+        all_dates = sorted(self.routing_db.smembers('imported_dates'), reverse=True)
+        if days_limit is not None:
+            all_dates = all_dates[:days_limit]
+        return [self.date_asn_block(ip, date) for date in all_dates]
 
-        :rtype: list. For each day in the database: day, asn, block
-    """
-    all_dates = sorted(get_db().smembers('imported_dates'), reverse=True)
-    if days_limit is not None:
-        all_dates = all_dates[:days_limit]
-    for date in all_dates:
-        yield date_asn_block(ip, date)
+    def aggregate_history(self, ip, days_limit=None):
+        """
+            Get the full history of an IP, aggregate the result instead of
+            returning one line per day.
 
+            :param ip: IP address to search for
+            :param days_limit: Max amount of days to query. (None means no limit)
 
-def aggregate_history(ip, days_limit=None):
-    """
-        Get the full history of an IP, aggregate the result instead of
-        returning one line per day.
-
-        :param ip: IP address to search for
-        :param days_limit: Max amount of days to query. (None means no limit)
-
-        :rtype: list. For each change: FirstDay, LastDay, ASN, Block
-    """
-    first_date = None
-    last_date = None
-    prec_asn = None
-    prec_block = None
-    for entry in history(ip, days_limit):
-        if entry is not None:
+            :rtype: list. For each change: FirstDay, LastDay, ASN, Block
+        """
+        first_date = None
+        last_date = None
+        prec_asn = None
+        prec_block = None
+        for entry in self.history(ip, days_limit):
+            if entry is None:
+                continue
             date, asn, block = entry
             if first_date is None:
                 last_date = date
@@ -217,5 +161,5 @@ def aggregate_history(ip, days_limit=None):
                 first_date = date
                 prec_asn = asn
                 prec_block = block
-    if first_date is not None:
-        yield first_date, last_date, prec_asn, prec_block
+        if first_date is not None:
+            yield first_date, last_date, prec_asn, prec_block
--- client/setup.py
+++ client/setup.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
 
 setup(
     name='ipasn-redis',
-    version='1.1',
+    version='2.0',
     description='API to access an IP-ASN-history instance via Redis.',
     url='https://github.com/CIRCL/IP-ASN-history',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
     packages=['ipasn_redis'],
-    scripts = ['bin/ipasn_redis', 'bin/ipasn-fullhistory_redis'],
+    scripts=['bin/ipasn_redis', 'bin/ipasn-fullhistory_redis'],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Development Status :: 5 - Production/Stable',
@@ -24,8 +24,8 @@ setup(
         'Topic :: Security',
         'Topic :: Internet',
         'Topic :: System :: Networking',
-        ],
+    ],
 
     install_requires=['redis']
 
-    )
+)
